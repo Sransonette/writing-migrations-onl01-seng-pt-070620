38afece8afb29c5459753d2c84abc3d64a7f6479
Sean Ransonette <sransonette@gmail.com>
2020-09-13 04:35:47 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :artists, :favorite_food, :string
+    add_column :grade, :integer, :birthdate, :string
   end
   
   
