6f26b15860652dc798dad8951c7abf3fa7f1c5e0
Sean Ransonette <sransonette@gmail.com>
2020-09-13 04:49:00 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,8 +1,10 @@
 class CreateStudents < ActiveRecord::Migration[5.2]
   def change
-    create_table :students do |t|
+    create_table :artists do |t|
       t.string :name
-      
+      t.string :genre
+      t.integer :age
+      t.string :hometown
     end
   end
   
