1cb010b1f354df163ad9baa33853780482f8a5c2
Sean Ransonette <sransonette@gmail.com>
2020-09-13 04:45:15 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2]
-  def change
-    add_column :grade, :integer, :birthdate, :string
+  def change_column
+    change_column :students, :birthdate, :datetime
   end
   
   
